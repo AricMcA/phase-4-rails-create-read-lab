f2b7c647e0e735850dbca709f938935d29bc3d05
Aric McAlister <aricmca@gmail.com>
2021-12-07 12:43:20 -0600

    commit tree 59725b713c490c64b05f95df06cd531ff3a04052

---
+++ app/models/plant.rb
@@ -0,0 +1,25 @@
+class Plant < ApplicationRecord
+    def index
+        render json: Plant.all, status: :ok
+    end
+
+    def show
+        plant = Plant.find_by(id: params[:id])
+        if plant
+            render json: plant, status: :ok
+        else
+            render json: {error: "plant not found"}, status: :not_found
+        end
+    end
+
+    def create
+        plant = Plant.create(plant_params)
+        render json: plant, status: :ok
+    end
+
+    private
+
+    def plant_params
+        params.permit(:name, :image, :price)
+    end
+end
